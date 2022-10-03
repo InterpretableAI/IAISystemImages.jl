ab3edded42afeb725b99442cecfb5ecaf9ca301c
iai <iai@iais-Mac-mini.local>
2022-10-03 14:19:52 -0400

    Update artifacts

--- src/install.jl
+++ src/install.jl
@@ -3,10 +3,4 @@
 @ArtifactInstaller(
     artifact"Julia_v1_8_2__IAI_v3_1_0",
     artifact"Julia_v1_8_2__IAI_dev",
-    artifact"Julia_v1_8_2__IAI_v3_1_0",
-    artifact"Julia_v1_8_2__IAI_dev",
-    artifact"Julia_v1_8_2__IAI_v3_1_0",
-    artifact"Julia_v1_8_2__IAI_dev",
-    artifact"Julia_v1_8_2__IAI_v3_1_0",
-    artifact"Julia_v1_8_2__IAI_dev",
 )
