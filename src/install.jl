86a265b233a09d5d5eab929dc9fdfca7c34a2834
iai <iai@iais-Mac-mini.local>
2022-10-21 20:55:17 -0400

    Update artifacts

--- src/install.jl
+++ src/install.jl
@@ -3,13 +3,6 @@
 @static if false
 elseif Sys.isapple() && Sys.ARCH == :x86_64
   @ArtifactInstaller(
-      artifact"Julia_v1_6_0__IAI_v2_2_0",
-      artifact"Julia_v1_6_0__IAI_v3_0_0",
-      artifact"Julia_v1_6_0__IAI_v3_0_1",
-      artifact"Julia_v1_6_0__IAI_v3_0_2",
-      artifact"Julia_v1_6_0__IAI_v3_1_0",
-      artifact"Julia_v1_6_0__IAI_v3_1_1",
-      artifact"Julia_v1_6_0__IAI_dev",
       artifact"Julia_v1_6_1__IAI_v2_2_0",
       artifact"Julia_v1_6_1__IAI_v3_0_0",
       artifact"Julia_v1_6_1__IAI_v3_0_1",
