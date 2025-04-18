ecbcba96198c090246b8eb14584100b2804f3199
iai <iai@iais-Mac-mini.local>
2025-04-18 02:31:17 -0400

    Update artifacts

--- src/install.jl
+++ src/install.jl
@@ -163,6 +163,8 @@ elseif Sys.isapple() && Sys.ARCH == :x86_64
       artifact"Julia_v1_11_3__IAI_dev",
       artifact"Julia_v1_11_4__IAI_v3_2_2",
       artifact"Julia_v1_11_4__IAI_dev",
+      artifact"Julia_v1_11_5__IAI_v3_2_2",
+      artifact"Julia_v1_11_5__IAI_dev",
   )
 elseif Sys.iswindows()
   @ArtifactInstaller(
@@ -333,6 +335,8 @@ elseif Sys.iswindows()
       artifact"Julia_v1_11_3__IAI_dev",
       artifact"Julia_v1_11_4__IAI_v3_2_2",
       artifact"Julia_v1_11_4__IAI_dev",
+      artifact"Julia_v1_11_5__IAI_v3_2_2",
+      artifact"Julia_v1_11_5__IAI_dev",
   )
 elseif Sys.isapple() && Sys.ARCH == :aarch64
   @ArtifactInstaller(
@@ -414,6 +418,8 @@ elseif Sys.isapple() && Sys.ARCH == :aarch64
       artifact"Julia_v1_11_3__IAI_dev",
       artifact"Julia_v1_11_4__IAI_v3_2_2",
       artifact"Julia_v1_11_4__IAI_dev",
+      artifact"Julia_v1_11_5__IAI_v3_2_2",
+      artifact"Julia_v1_11_5__IAI_dev",
   )
 elseif Sys.islinux()
   @ArtifactInstaller(
@@ -584,5 +590,7 @@ elseif Sys.islinux()
       artifact"Julia_v1_11_3__IAI_dev",
       artifact"Julia_v1_11_4__IAI_v3_2_2",
       artifact"Julia_v1_11_4__IAI_dev",
+      artifact"Julia_v1_11_5__IAI_v3_2_2",
+      artifact"Julia_v1_11_5__IAI_dev",
   )
 end
